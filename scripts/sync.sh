8684254de29cd888c022d33ea6514450eab20b80
ad432952 <a.m.derylo@gmail.com>
2023-06-04 10:24:11 +0200

    Fomrating file and update scripts

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -4,5 +4,7 @@ cd "$(dirname "$0")" # set current working directory to the directory of the scr
 cd ..
 
 diff -rupNEZbB minix_source/usr/ solution/usr/ > ab123456.patch
+
+ssh-copy-id -i ~/.ssh/minix.pub -o IdentitiesOnly=yes minix
 scp ab123456.patch minix:/
-scp scripts/apply.sh minix:/
+scp -r tests minix:/
